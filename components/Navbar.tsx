01bc87e4dc6a178ae11ca71448bd94371784deda
Manson <manson972@yahoo.com>
2023-07-23 19:11:20 +0800

    update Navbar and constants, AuthProvider

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import { NavLinks } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import AuthProvider from './AuthProvider';
 
 const Navbar = () => {
+    const session = {};
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav className="flexBetween navbar">
+            <div className="flex-1 flexStart gap-10">
+                <Link href="/">
+                    <Image 
+                        src="/Logo.svg"
+                        width={115}
+                        height={43}
+                        alt="Flexibble" 
+                    />
+                </Link>
+                <ul className="xl:flex hidden text-small gap-7">
+                    { NavLinks.map((link) => {
+                        return (
+                            <Link href={link.href} key={link.key}>
+                                {link.text}
+                            </Link>
+                        )
+                    }) }
+                </ul>
+            </div>
+            <div className="flexCenter gap-4">
+                { session 
+                ? (
+                    <>
+                        UserPhoto
+                        <Link href="/create-project">
+                            Share Work
+                        </Link>
+                    </>
+                ) 
+                : (
+                    <>
+                        <AuthProvider />
+                    </>
+                ) 
+                }
+            </div>
+        </nav>
     );
 };
 
